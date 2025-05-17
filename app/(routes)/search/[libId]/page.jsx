07bffc9bbd6c426b7f5f95ec90dcdd51a9dd0584
Ansh Sharma <anshsharma2463@gmail.com>
2025-05-17 20:51:31 +0530

    Pages

--- app/(routes)/search/[libId]/page.jsx
+++ app/(routes)/search/[libId]/page.jsx
@@ -3,6 +3,8 @@ import { supabase } from '@/services/supabase'
 import { useParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import Header from './_components/Header'
+import DisplayResult from './_components/DisplayResult'
+
 
 function SearchQueryResult() {
     const {libId}=useParams()
@@ -21,7 +23,9 @@ function SearchQueryResult() {
     return (
         <div>
             <Header searchInputRecord={searchInputRecord}/>
-            <DisplayResult searchInputRecord={searchInputRecord}/>
+            <div className='px-10 md:px-20 lg:px-36 xl:px-56 mt-20'>
+                <DisplayResult searchInputRecord={searchInputRecord}/>
+            </div>
         </div>
     )
 }
